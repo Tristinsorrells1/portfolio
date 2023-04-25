c99fa30bcc428357d14cf6076c5070a59bfbf102
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-04-25 16:49:09 -0500

    Create initial header layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from "react-router"
+import Home from "./Home"
+import Header from "./Header"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <main>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </React.StrictMode>
+  </BrowserRouter>
 )
